a2b407fef3fcb5c1a79324ebfa93a5c8cdafe3cb
gekac <37373492+Bouboukis@users.noreply.github.com>
2020-11-05 09:07:26 +0100

    The integer file is to tache you about the basic of using integers and declaring them in Java

---
+++ src/integer.java
@@ -0,0 +1,11 @@
+class integer {
+
+    public static void main(String[] args) {
+
+        int x;
+        int y = 8;
+        x = 5;
+        int z = x + y;
+        System.out.println(z);
+    }
+}
