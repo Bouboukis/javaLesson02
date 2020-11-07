d369b68f9eab7764ccd04be9e5041d12e5730e9e
gekac <37373492+Bouboukis@users.noreply.github.com>
2020-11-07 09:12:53 +0100

    Double and float numbers lesson.

---
+++ src/real.java
@@ -0,0 +1,15 @@
+public class real {
+    public static void main(String[] args){
+
+        double real01, real02;
+        real01 = 25.223;
+        real02 = 102546.236954;
+        System.out.println(real01 * real02);
+
+        // Do not use floats, only if you have memory constrains
+        float real03 ;
+        real03 = 0.4f;
+        real03 = real03 * 2;
+        System.out.println(real03);
+    }
+}
