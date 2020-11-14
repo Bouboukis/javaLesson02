30d2db5c44d3b64f5e10ac67acf337faafdcdb4d
gekac <37373492+Bouboukis@users.noreply.github.com>
2020-11-14 17:47:25 +0100

    Merge remote-tracking branch 'origin/main' into main
    # Conflicts:
    #	README.md

---
+++ src/excercise01.java
@@ -0,0 +1,18 @@
+public class excercise01 {
+    public static void main(String[] args) {
+        double pi = 3.14;
+        System.out.println(pi);
+        long l = 149L;
+        System.out.println(l);
+        boolean b= true;
+        System.out.println(b);
+        double d = 1E0;
+        System.out.println(d);
+        int i = 899;
+        System.out.println(i);
+        int x = 0xA0;
+        System.out.println(x);
+        char c = '\u0041';
+        System.out.println(c);
+    }
+}
